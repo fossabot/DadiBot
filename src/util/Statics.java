61dce693c72623255c41cf14834b7ed307cd57d3
EweLoHD <ewelo230@outlook.com>
2018-01-04 18:55:14 +0100

    Delete Statics.java

--- src/util/Statics.java
+++
@@ -1,8 +0,0 @@
-package util;
-
-public class Statics {
-
-	public static final String prefix = "!";
-	public static final String token = "Mzk2OTkwMjE4NzUxOTY3MjU0.DSqXsQ.GY-bxVKGaCzmHHuh34kda5vsccA";
-	
-}
